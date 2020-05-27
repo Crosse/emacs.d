b14fe684037eef7db7334d3bd32584c61318d9d6
Seth Wright <seth@crosse.org>
2020-05-27 10:14:26 -0400

    let emacs do what it wants to custom.el

--- custom.el
+++ custom.el
@@ -5,20 +5,22 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(TeX-auto-save t)
- '(TeX-parse-self t)
- '(TeX-save-query nil)
+ '(TeX-auto-save t t)
+ '(TeX-parse-self t t)
+ '(TeX-save-query nil t)
  '(custom-safe-themes
-   '("251348dcb797a6ea63bbfe3be4951728e085ac08eee83def071e4d2e3211acc3" "a2cde79e4cc8dc9a03e7d9a42fabf8928720d420034b66aecc5b665bbf05d4e9" "1b8d67b43ff1723960eb5e0cba512a2c7a2ad544ddb2533a90101fd1852b426e" default))
- '(lsp-auto-guess-root t)
- '(lsp-enable-snippet nil)
- '(lsp-ui-sideline-delay 1.5)
- '(lsp-ui-sideline-ignore-duplicate t)
- '(lsp-ui-sideline-show-code-actions nil)
- '(lsp-ui-sideline-show-hover nil)
- '(lsp-ui-sideline-show-symbol nil)
+   (quote
+    ("f9aede508e587fe21bcfc0a85e1ec7d27312d9587e686a6f5afdbb0d220eab50" "251348dcb797a6ea63bbfe3be4951728e085ac08eee83def071e4d2e3211acc3" "a2cde79e4cc8dc9a03e7d9a42fabf8928720d420034b66aecc5b665bbf05d4e9" "1b8d67b43ff1723960eb5e0cba512a2c7a2ad544ddb2533a90101fd1852b426e" default)))
+ '(lsp-auto-guess-root t t)
+ '(lsp-enable-snippet nil t)
+ '(lsp-ui-sideline-delay 1.5 t)
+ '(lsp-ui-sideline-ignore-duplicate t t)
+ '(lsp-ui-sideline-show-code-actions nil t)
+ '(lsp-ui-sideline-show-hover nil t)
+ '(lsp-ui-sideline-show-symbol nil t)
  '(package-selected-packages
- '(airline-themes all-the-icons auctex auctex-latexmk auto-compile cargo cider clojure-mode cmake-font-lock cmake-mode comment-dwim-2 company company-auctex company-lsp diff-hl doom-modeline editorconfig evil evil-numbers exec-path-from-shell flycheck flycheck-clangcheck go-mode groovy-mode helm helm-company hl-todo lsp-mode lsp-ui markdown-mode minions monokai-theme paredit pdf-tools platformio-mode powerline-evil projectile rainbow-blocks rainbow-delimiters rust-mode slime-repl-ansi-color sly sly-asdf sly-quicklisp smartparens use-package yaml-mode))
+   (quote
+    (paredit crystal-mode powershell yaml-mode cider clojure-mode pdf-tools smartparens auctex auctex-latexmk company-auctex sly-asdf rainbow-blocks rainbow-delimiters company exec-path-from-shell markdown-mode platformio-mode sly sly-quicklisp auto-compile flycheck-clangcheck cmake-mode cmake-font-lock hl-todo flycheck go-mode projectile groovy-mode all-the-icons lsp-ui company-lsp lsp-mode cargo rust-mode comment-dwim-2 diff-hl doom-modeline monokai-theme use-package slime-repl-ansi-color powerline-evil minions helm helm-company evil evil-numbers editorconfig airline-themes)))
  '(rust-format-on-save t t))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
