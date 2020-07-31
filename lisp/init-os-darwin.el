97305d1c4c89e0f3f63dd6111c6c04d30848ba74
Seth Wright <seth@crosse.org>
2020-07-31 18:58:23 -0400

    Prettier things on macOS

--- lisp/init-os-darwin.el
+++ lisp/init-os-darwin.el
@@ -10,5 +10,9 @@
     (setq exec-path-from-shell-check-startup-files nil)
     (exec-path-from-shell-initialize)))
 
+; Prettier title bar.
+(add-to-list 'default-frame-alist'(ns-transparent-titlebar . t))
+(add-to-list 'default-frame-alist'(ns-appearance . light))
+
 (provide 'init-os-darwin)
 ;;; init-os-darwin.el ends here
