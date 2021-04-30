68df12e06e6c7060391b9ce2d734552cec01758a
Seth Wright <seth.wright@fireeye.com>
2021-04-30 11:29:59 -0400

    Update custom.el

--- custom.el
+++ custom.el
@@ -8,14 +8,11 @@
  '(TeX-auto-save t)
  '(TeX-parse-self t)
  '(TeX-save-query nil)
- '(custom-safe-themes
-   '("8b58ef2d23b6d164988a607ee153fd2fa35ee33efc394281b1028c2797ddeebb"))
  '(helm-minibuffer-history-key "M-p")
- '(lsp-completion-provider t nil nil "Customized with use-package lsp-mode")
  '(lsp-solargraph-library-directories
    '("~/.rbenv/" "/usr/lib/ruby/" "~/.rvm/" "~/.gem/" "/opt/pkg/lib/ruby"))
  '(package-selected-packages
-   '(evil-paredit jenkins jenkinsfile-mode airline-themes all-the-icons auctex auctex-latexmk auto-compile cargo cider clojure-mode cmake-font-lock cmake-mode comment-dwim-2 company company-auctex company-box company-lsp crystal-mode diff-hl discover-my-major dockerfile-mode doom-modeline editorconfig ember-mode esup evil evil-collection evil-goggles exec-path-from-shell flycheck flycheck-clangcheck flycheck-rust gcmh gnutls go-mode groovy-mode handlebars-mode helm helm-company helpful hl-todo latex lsp-java lsp-mode lsp-ui magit markdown-mode minions monokai-theme nix-mode paredit pdf-tools platformio-mode powerline-evil powershell projectile rainbow-blocks rainbow-delimiters rust-mode slime-repl-ansi-color sly sly-asdf sly-quicklisp smartparens terraform-mode use-package yaml-mode)))
+   '(json-mode shfmt evil-paredit jenkinsfile-mode airline-themes all-the-icons auctex auctex-latexmk auto-compile cargo cider clojure-mode cmake-font-lock cmake-mode comment-dwim-2 company company-auctex company-box company-lsp crystal-mode diff-hl discover-my-major dockerfile-mode doom-modeline editorconfig esup evil evil-collection evil-goggles exec-path-from-shell flycheck flycheck-clangcheck flycheck-rust gcmh gnutls go-mode groovy-mode helm helm-company helpful hl-todo latex lsp-java lsp-mode lsp-ui magit markdown-mode minions monokai-theme paredit platformio-mode powerline-evil powershell projectile rainbow-delimiters rustic slime-repl-ansi-color sly sly-asdf sly-quicklisp treemacs treemacs-evil terraform-mode undo-tree use-package vimrc-mode yaml-mode)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
