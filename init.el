961d2ba006c7a6feee8217f573f5e1da2a5deaa1
Seth Wright <seth@crosse.org>
2023-04-14 13:13:10 -0400

    Remove Helm and replace with Vertico, Marginalia, Consult, and Embark

--- init.el
+++ init.el
@@ -376,30 +376,145 @@
   :config (minions-mode 1))
 
 
+;;; leaving this in but commented out just because it's my file and I'll do what I want
+
 ;; Emacs incremental completion and selection narrowing framework
 ;; https://github.com/emacs-helm/helm
-(use-package helm
-  :config
-  (helm-mode 1)
-  ;; Rebind M-x to use Helm mode.
-  (global-set-key (kbd "M-x") 'helm-M-x)
-  ;; Remap various functions to the Helm equivalent
-  (define-key global-map [remap find-file] 'helm-find-files)
-  (define-key global-map [remap occur] 'helm-occur)
-  (define-key global-map [remap list-buffers] 'helm-buffers-list)
-  (define-key global-map [remap dabbrev-expand] 'helm-dabbrev)
-  (define-key global-map [remap execute-extended-command] 'helm-M-x)
-  (unless (boundp 'completion-in-region-function)
-    (define-key lisp-interaction-mode-map [remap completion-at-point] 'helm-lisp-completion-at-point)
-    (define-key emacs-lisp-mode-map       [remap completion-at-point] 'helm-lisp-completion-at-point))
+;; (use-package helm
+;;   :config
+;;   (helm-mode 1)
+;;   ;; Rebind M-x to use Helm mode.
+;;   (global-set-key (kbd "M-x") 'helm-M-x)
+;;   ;; Remap various functions to the Helm equivalent
+;;   (define-key global-map [remap find-file] 'helm-find-files)
+;;   (define-key global-map [remap occur] 'helm-occur)
+;;   (define-key global-map [remap list-buffers] 'helm-buffers-list)
+;;   (define-key global-map [remap dabbrev-expand] 'helm-dabbrev)
+;;   (define-key global-map [remap execute-extended-command] 'helm-M-x)
+;;   (unless (boundp 'completion-in-region-function)
+;;     (define-key lisp-interaction-mode-map [remap completion-at-point] 'helm-lisp-completion-at-point)
+;;     (define-key emacs-lisp-mode-map       [remap completion-at-point] 'helm-lisp-completion-at-point))
 
+;;   :custom
+;;   ;; Use 'rg' instead of 'ag' in Helm
+;;   (helm-grep-ag-command "rg --color=always --colors 'match:fg:black' --colors 'match:bg:yellow' --smart-case --no-heading --line-number %s %s %s")
+;;   (helm-grep-ag-pipe-cmd-switches '("--colors 'match:fg:black'" "--colors 'match:bg:yellow'"))
+;;   (helm-mode-fuzzy-match t)
+;;   (helm-completion-in-region-fuzzy-match t))
+
+
+;; VERTical Interactive COmpletion
+;; https://github.com/minad/vertico
+(use-package vertico
+  :init
+  (vertico-mode)
+  (vertico-mouse-mode)
+  (setq vertico-cycle t)
+  :bind (:map minibuffer-local-map
+          ("C-l" . vertico-directory-up))
+  :hook (rfn-eshadow-update-overlay . vertico-directory-tidy))
+
+
+;; Emacs completion style that matches multiple regexps in any order
+;; https://github.com/oantolin/orderless
+(use-package orderless
+  :init
+  (setq
+    completion-category-defaults nil
+    completion-ignore-case t)
   :custom
-  ;; Use 'rg' instead of 'ag' in Helm
-  (helm-grep-ag-command "rg --color=always --colors 'match:fg:black' --colors 'match:bg:yellow' --smart-case --no-heading --line-number %s %s %s")
-  (helm-grep-ag-pipe-cmd-switches '("--colors 'match:fg:black'" "--colors 'match:bg:yellow'"))
-  (helm-mode-fuzzy-match t)
-  (helm-completion-in-region-fuzzy-match t))
+  (completion-styles '(substring orderless basic))
+  (completion-category-overrides '((file (styles basic partial-completion))))
+  (read-file-name-completion-ignore-case t)
+  (read-buffer-completion-ignore-case t))
 
+(use-package savehist
+  :ensure nil
+  :init (savehist-mode))
+
+(use-package crm
+  :ensure nil
+  :init
+  ;; Add prompt indicator to `completing-read-multiple'.
+  ;; We display [CRM<separator>], e.g., [CRM,] if the separator is a comma.
+  (defun crm-indicator (args)
+    (cons (format "[CRM%s] %s"
+            (replace-regexp-in-string
+              "\\`\\[.*?]\\*\\|\\[.*?]\\*\\'" ""
+              crm-separator)
+            (car args))
+      (cdr args)))
+  (advice-add #'completing-read-multiple :filter-args #'crm-indicator))
+
+(use-package emacs
+  :ensure nil
+  :init
+  ;; Do not allow the cursor in the minibuffer prompt
+  (setq minibuffer-prompt-properties
+    '(read-only t cursor-intangible t face minibuffer-prompt))
+  (add-hook 'minibuffer-setup-hook #'cursor-intangible-mode)
+
+  ;; Emacs 28: Hide commands in M-x which do not work in the current mode.
+  ;; Vertico commands are hidden in normal buffers.
+  (setq read-extended-command-predicate
+    #'command-completion-default-include-p)
+
+  ;; Enable recursive minibuffers
+  (setq enable-recursive-minibuffers t))
+
+;; Enable rich annotations using the Marginalia package
+;; https://github.com/minad/marginalia/
+(use-package marginalia
+  :bind (("M-A" . marginalia-cycle)
+          :map minibuffer-local-map
+          ("M-A" . marginalia-cycle))
+
+  :init (marginalia-mode))
+
+(use-package recentf
+  :ensure nil
+  :init (recentf-mode))
+
+(use-package xref :ensure nil)
+
+;; Consulting completing-read
+;; https://github.com/minad/consult/
+;; TODO: check out consult-line{,-multi}, consult-xref, consult-{,rip}grep
+(use-package consult
+  :requires (xref recentf)
+  :custom
+  (xref-show-xrefs-function #'consult-xref)
+  (xref-show-definitions-function #'consult-xref)
+  (consult-project-function (lambda (_) (projectile-project-root)))
+
+  :config
+  (define-key global-map [remap switch-to-buffer] 'consult-buffer)
+  (define-key global-map [remap switch-to-buffer-other-frame] 'consult-buffer-other-frame)
+  (define-key global-map [remap switch-to-buffer-other-window] 'consult-buffer-other-window)
+
+  :hook (completion-list-mode . consult-preview-at-point-mode))
+
+
+;; Emacs Mini-Buffer Actions Rooted in Keymaps
+;; https://github.com/oantolin/embark
+(use-package embark
+  :bind
+  (("C-." . embark-act)
+    ("C-;" . embark-dwim)
+    ("C-h B" . embark-bindings))
+  :config
+  ;; Hide the mode line of the Embark live/completions buffers
+  (add-to-list 'display-buffer-alist
+               '("\\`\\*Embark Collect \\(Live\\|Completions\\)\\*"
+                 nil
+                 (window-parameters (mode-line-format . none)))))
+
+
+;; Embark-Consult integration
+;; Also in https://github.com/oantolin/embark
+(use-package embark-consult
+  :requires embark
+  :hook (embark-collect-mode . consult-preview-at-point-mode))
 
 ;; Package for highlighting uncommitted changes
 ;; https://github.com/dgutov/diff-hl
