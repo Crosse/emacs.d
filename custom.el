6a0955d0de8416ea81e094ab05c4b68e27a28a46
Seth Wright <seth@crosse.org>
2023-04-14 13:25:08 -0400

    Bring custom.el up to reality

--- custom.el
+++ custom.el
@@ -8,17 +8,12 @@
  '(TeX-auto-save t)
  '(TeX-parse-self t)
  '(TeX-save-query nil)
- '(column-number-mode t)
- '(global-display-line-numbers-mode t)
  '(helm-minibuffer-history-key "M-p")
  '(ignored-local-variable-values '((engine . jsx)))
  '(lsp-solargraph-library-directories
    '("~/.rbenv/" "/usr/lib/ruby/" "~/.rvm/" "~/.gem/" "/opt/pkg/lib/ruby"))
  '(package-selected-packages
-   '(csharp-mode ssh-config-mode pest-mode rjsx-mode web-mode toc-org color-theme-sanityinc-tomorrow-colors command-log-mode autumn-light-theme mindre-theme color-theme-sanityinc-tomorrow color-theme-sanityinc-solarized tmux-pane window-numbering pixel-scroll fira-code-mode vterm eterm-256color elixir-mode wakatime-mode flycheck-golangci-lint graphviz-dot-mode kotlin-mode bnf-mode bundler rufo sphinx-doc poetry python-isort blacken json-mode shfmt evil-paredit jenkinsfile-mode airline-themes all-the-icons auctex auctex-latexmk auto-compile cargo cider clojure-mode cmake-font-lock cmake-mode comment-dwim-2 company company-auctex company-box company-lsp crystal-mode diff-hl discover-my-major dockerfile-mode doom-modeline editorconfig esup evil evil-collection evil-goggles exec-path-from-shell flycheck flycheck-clangcheck flycheck-rust gcmh gnutls go-mode groovy-mode helm helm-company helpful hl-todo latex lsp-java lsp-mode lsp-ui magit markdown-mode minions monokai-theme paredit platformio-mode powerline-evil powershell projectile rainbow-delimiters rustic slime-repl-ansi-color sly sly-asdf sly-quicklisp terraform-mode undo-tree use-package vimrc-mode yaml-mode))
- '(show-paren-mode t)
- '(size-indication-mode t)
- '(tool-bar-mode nil))
+   '(embark-consult embark vertico-directory consult marginalia vertico all-the-icons-dired which-key projectile-ripgrep ripgrep exercism evil-cleverparens smartparens protobuf-mode aggressive-indent elisp-autofmt perspective csharp-mode ssh-config-mode pest-mode rjsx-mode web-mode toc-org color-theme-sanityinc-tomorrow-colors command-log-mode autumn-light-theme mindre-theme color-theme-sanityinc-tomorrow color-theme-sanityinc-solarized tmux-pane window-numbering pixel-scroll fira-code-mode vterm eterm-256color elixir-mode wakatime-mode flycheck-golangci-lint graphviz-dot-mode kotlin-mode bnf-mode bundler rufo sphinx-doc poetry python-isort blacken json-mode shfmt evil-paredit jenkinsfile-mode airline-themes all-the-icons auctex auctex-latexmk auto-compile cargo cider clojure-mode cmake-font-lock cmake-mode comment-dwim-2 company company-auctex company-box company-lsp crystal-mode diff-hl discover-my-major dockerfile-mode doom-modeline editorconfig esup evil evil-collection evil-goggles exec-path-from-shell flycheck flycheck-clangcheck flycheck-rust gcmh gnutls go-mode groovy-mode helm helm-company helpful hl-todo latex lsp-java lsp-mode lsp-ui magit markdown-mode minions monokai-theme paredit platformio-mode powerline-evil powershell projectile rainbow-delimiters rustic slime-repl-ansi-color sly sly-asdf sly-quicklisp terraform-mode undo-tree use-package vimrc-mode yaml-mode)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
